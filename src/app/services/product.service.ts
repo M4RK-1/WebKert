5c594295dfb40838f154118393ec18030872a5d1
M4RK-1 <vizimark@gmail.com>
2024-05-19 21:58:04 +0200

    AAAAAAAA

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,15 +1,17 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Observable } from 'rxjs';
 import { Product } from '../models/product.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
+
   constructor(private firestore: AngularFirestore) {}
 
-  getProducts() {
-    return this.firestore.collection('products').snapshotChanges();
+  getProducts(): Observable<Product[]> {
+    return this.firestore.collection<Product>('products').valueChanges({ idField: 'id' });
   }
 
   getProductsByCategory(category: string) {
@@ -18,15 +20,7 @@ export class ProductService {
     ).snapshotChanges();
   }
 
-  addProduct(product: Product) {
-    return this.firestore.collection('products').add(product);
-  }
-
-  updateProduct(id: string, product: Product) {
-    return this.firestore.doc(`products/${id}`).update(product);
-  }
-
-  deleteProduct(id: string) {
-    return this.firestore.doc(`products/${id}`).delete();
+  getProductById(id: string): Observable<Product | undefined> {
+    return this.firestore.collection<Product>('products').doc(id).valueChanges();
   }
 }
